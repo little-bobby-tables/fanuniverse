5dedfdcf71e2ed8eb1d1a9399f727cd8beacd693
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-02-03 20:02:02 +0700

    users_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_permitted_parameters_for_devise, if: :devise_controller?
+
+  protected
+
+  def set_permitted_parameters_for_devise
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: User::ALLOWED_PARAMETERS)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'feed#index'
 
   resources :images, constraints: { id: /\d+/ }
+
+  resources :users, param: :name, except: [:new, :create, :destroy]
 end
