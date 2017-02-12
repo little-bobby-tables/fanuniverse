11c3b5c065ecab58cf4df86d6869650f6c0e8a2a
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-02-12 09:23:09 +0700

    user menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,11 @@
 import stars from './stars';
 import upload from './upload';
+import header from './header';
 
 function load() {
   stars();
   upload();
+  header();
 }
 
 if (document.readyState !== 'loading') load();
