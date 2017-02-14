41be75e56372e8ff3cf445d0933de99de64a6a57
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-02-14 07:18:36 +0700

    ajax comment postin

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -3,18 +3,47 @@ import { timeago } from './timeago';
 export default function() {
   const commentable = document.querySelector('[data-commentable]');
 
-  commentable && loadCommentable(commentable);
+  if (commentable) {
+    loadCommentable(commentable);
+    setupAjaxPosting(commentable);
+  }
 }
 
-const endpont = (type, id) => `/comments?commentable_type=${type}&commentable_id=${id}`;
+const endpoint = (type, id) => `/comments?commentable_type=${type}&commentable_id=${id}`;
 
-function loadCommentable(element) {
-  const { commentable, commentableId } = element.dataset;
+function loadCommentable(container) {
+  const { commentable, commentableId } = container.dataset;
 
-  fetch(endpont(commentable, commentableId), { credentials: 'same-origin' })
+  fetch(endpoint(commentable, commentableId), { credentials: 'same-origin' })
       .then(response => response.text())
-      .then(comments => {
-        element.innerHTML = comments;
-        timeago(element);
-      });
+      .then(comments => displayComments(container, comments));
+}
+
+function displayComments(container, comments) {
+  container.innerHTML = comments;
+  timeago(container);
+}
+
+function setupAjaxPosting(container) {
+  /* Form submission is handled by rails.js, we only need to display the new comments. */
+  document.addEventListener('ajax:success', (e) => {
+    if (e.target.id === 'js-commentable-form') commentPosted(container, e.detail);
+  });
+}
+
+function commentPosted(container, response) {
+  const commentEditArea = document.querySelector('#js-commentable-form textarea'),
+        ajaxRequestOk   = response[2].status === 200,
+        responseBody    = response[2].responseText;
+
+  commentEditArea.value = '';
+
+  if (ajaxRequestOk) {
+    displayComments(container, responseBody);
+  }
+  else {
+    /* Error message will be displayed on reload at the top of the page (flash). */
+    window.location.reload();
+    window.scrollTo(0, 0);
+  }
 }
