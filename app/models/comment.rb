6676826b5bc87e949fdb5077754d7a5fe106b10d
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-02-15 17:26:04 +0700

    style

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   COMMENTABLE = %w(Image User).freeze
 
+  default_scope { order(created_at: :desc) }
+
   belongs_to :commentable, polymorphic: true, counter_cache: 'comment_count'
   belongs_to :author, class_name: 'User', inverse_of: :authored_comments
 
