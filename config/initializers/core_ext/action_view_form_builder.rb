4df0eb9f020948abc077a9d8dc8b520100a84202
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-02-23 10:51:25 +0700

    devise views

---
+++ config/initializers/core_ext/action_view_form_builder.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+class ActionView::Helpers::FormBuilder
+  def styled_check_box(method, text = method.to_s.humanize, checkbox_options = {})
+    @template.content_tag(:div, class: 'checkbox') do
+      checkbox_options[:class] = 'checkbox__default-input ' + checkbox_options[:class].to_s
+      check_box(method, checkbox_options) +
+        label(method, class: 'checkbox__label') do
+          @template.content_tag(:span, class: 'checkbox__text') { text }
+        end
+    end
+  end
+end
+
+module ActionView::Helpers::FormTagHelper
+  def styled_check_box_tag(name, text = name.to_s.humanize, value = "1", checked = false, checkbox_options = {})
+    content_tag(:div, class: 'checkbox') do
+      checkbox_options[:class] = 'checkbox__default-input ' + checkbox_options[:class].to_s
+      check_box_tag(name, value, checked, checkbox_options) +
+        label_tag(name, class: 'checkbox__label') do
+          content_tag(:span, class: 'checkbox__text') { text }
+        end
+    end
+  end
+end
