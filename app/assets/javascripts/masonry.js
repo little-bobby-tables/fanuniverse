904c008102981c8fbb255afc063bed2ca525f423
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-03-16 22:59:06 +0700

    display image grid while it's still loading
    This makes the site more tolerable on slower connections,
    since the progress can be easily perceived now compared to
    a blank page that used to be there.

--- app/assets/javascripts/masonry.js
+++ app/assets/javascripts/masonry.js
@@ -1,30 +1,36 @@
 export default function() {
-  const grid = document.querySelector('.js-masonry-grid'),
-        header = grid && document.querySelector('.js-masonry-grid-header');
+  const grid    = document.querySelector('.js-masonry-grid'),
+        masonry = grid && masonryjs(grid),
+        imgload = grid && imagesLoaded(grid);
 
-  /* Initializes Masonry after all images have loaded —
-   * see http://masonry.desandro.com/layout.html#imagesloaded */
+  /* "Unloaded images can throw off Masonry layouts and cause item elements to overlap",
+   * see http://masonry.desandro.com/layout.html#imagesloaded. */
 
-  grid && imagesLoaded(grid, () => {
-    const masonry = new Masonry(grid, {
-      itemSelector: '.grid__item',
-      transitionDuration: '0.2s',
-      isFitWidth: true,
-      initLayout: false
-    });
+  imgload && imgload.on('progress', () => masonry.layout());
+}
 
-    /* Resizes .js-masonry-grid-header to appear to be the same width as the grid.
-     * This way we can center the grid in page without having its header dangling off the side. */
-
-    masonry.on('layoutComplete', () => {
-      const gridWidth = (masonry.cols * masonry.columnWidth);
-
-      header.style.width = `${gridWidth}px`;
-
-      grid.classList.remove('grid--loading');
-      header.classList.remove('grid--loading');
-    });
-
-    masonry.layout();
+function masonryjs(grid) {
+  const masonry = new Masonry(grid, {
+    itemSelector: '.grid__item',
+    transitionDuration: '0.2s',
+    isFitWidth: true,
+    initLayout: false /* delay layout() to bind the layoutComplete listener */
   });
+
+  const header    = document.querySelector('.js-masonry-grid-header'),
+        container = document.querySelector('.js-grid-loading');
+
+  masonry.on('layoutComplete', () => {
+    const gridWidth = (masonry.cols * masonry.columnWidth);
+    header.style.width = `${gridWidth}px`;
+  });
+
+  masonry.layout();
+
+  /* .js-grid-loading makes the element invisible. We only remove this class
+   * after setting the header width; this prevents layout "jumps" caused by
+   * undetermined width coupled with margin: auto. */
+  container.classList.remove('js-grid-loading');
+
+  return masonry;
 }
