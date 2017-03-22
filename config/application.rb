335ac59b8dbf0413b9956175635df278590af0dd
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-03-22 23:18:12 +0700

    remove unneeded module from application config

--- config/application.rb
+++ config/application.rb
@@ -6,25 +6,23 @@ require 'slim/include'
 
 Bundler.require(*Rails.groups)
 
-module StevenOnRails
-  class Application < Rails::Application
-    config.active_job.queue_adapter = :sidekiq
+class Application < Rails::Application
+  config.active_job.queue_adapter = :sidekiq
 
-    config.eager_load_paths << Rails.root.join('lib')
-    config.eager_load_paths << Rails.root.join('app', 'models', 'validators')
+  config.eager_load_paths << Rails.root.join('lib')
+  config.eager_load_paths << Rails.root.join('app', 'models', 'validators')
 
-    config.assets.enabled = false
-    config.assets.compile = false
+  config.assets.enabled = false
+  config.assets.compile = false
 
-    Slim::Engine.set_options format: :html
+  Slim::Engine.set_options format: :html
 
-    config.generators do |g|
-      g.assets false
-      g.view_specs true
-      g.helper_specs false
-      g.orm :active_record
-      g.fixture_replacement :factory_girl
-      g.javascript_engine :js
-    end
+  config.generators do |g|
+    g.assets false
+    g.view_specs true
+    g.helper_specs false
+    g.orm :active_record
+    g.fixture_replacement :factory_girl
+    g.javascript_engine :js
   end
 end
