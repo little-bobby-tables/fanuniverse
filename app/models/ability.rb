b6fee4c528c41a2bbdff1e79ecfe6eb444786a89
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-04-11 17:18:48 +0700

    adjust Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,8 @@ class Ability
   def initialize(user)
     allow_guest_user_actions
     allow_logged_in_user_actions(user) if user
+
+    allow_anything! if user.administrator?
   end
 
   def allow_guest_user_actions
@@ -15,9 +17,13 @@ class Ability
   def allow_logged_in_user_actions(_user)
     can :create, Image
     can :edit, Image
+
     can :comment_on, Image
     can :comment_on, Profile
+
     can :star, Image
     can :star, Comment
+
+    can :report, Image
   end
 end
